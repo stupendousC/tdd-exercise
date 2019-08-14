fa3145fac2f60bdf86433d966ec3501d15ecbc84
Caroline <CarolineWuKaplan@gmail.com>
2019-08-13 22:14:49 -0700

    blackjack_score works but blackjack_score_test is giving red bars

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,53 @@
-# blackjack_score.rb
-
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
-
+  
+  unless hand.class == Array
+    raise ArgumentError#.new("I NEED AN ARRAY")
+  end
+  
+  if hand.length > 5
+    raise ArgumentError#.new("TOO MANY CARDS")
+  elsif hand.length < 2
+    raise ArgumentError#.new("I NEED AT LEAST 2 CARDS")
+  end
+  
+  # add up scores
+  score_hash = { 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 10 => 10, 'Jack' => 10, 'Queen' => 10, 'King' => 10, 'Ace' => [1,11] }
+  score = 0
+  qty_aces = 0
+  
+  hand.each do |card|
+    unless VALID_CARDS.include? card
+      # does not account for case discrepancies (need .capitalize)
+      raise ArgumentError#.new("INVALID CARD: #{card}")
+      
+    else 
+      # assign numerical value and add to running score
+      if card == "Ace"
+        # set aside for now, decide on value at the end
+        qty_aces += 1
+        next
+      else
+        value = score_hash[card]
+        score += value
+      end
+    end
+  end
+  
+  # decide on value(s) of Ace(s)
+  qty_aces.times do |index|
+    if score + 10 <= (20 - index)    # b/c 21-(index+1) = 20
+      score += 10
+    else
+      score += 1
+    end
+  end
+  
+  if score > 21
+    raise ArgumentError#.new("BUST!")
+  end
+  
+  return score
 end
+
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -6,38 +6,43 @@ require 'minitest/pride'
 
 require_relative '../lib/blackjack_score'
 
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+Minitest::Reporters.use! 
+Minitest::Reporters::SpecReporter.new
 
 describe 'Blackjack Score' do
   it 'can calculate the score for a pair of number cards' do
-
-    # Arrange
     hand = [3, 4]
-
-    # Act
     score = blackjack_score(hand)
-
-    # Assert <-  You do this part!
-
+    expect(score).must_equal 7
   end
-
+  
   it 'facecards have values calculated correctly' do
-
+    hand = [3, 'Jack']
+    score = blackjack_score(hand)
+    expect(score).must_equal 13
   end
-
+  
   it 'calculates aces as 11 where it does not go over 21' do
-
+    hand = ['Ace', 10, 10]
+    score = blackjack_score(hand)
+    expect(score).must_equal 21
   end
-
+  
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
-
+    hand = ['Ace', 'Jack', 9]
+    score = blackjack_score(hand)
+    expect(score).must_equal 20
   end
-
+  
   it 'raises an ArgumentError for invalid cards' do
-
+    hand = ['JOKER']
+    score = blackjack_score(hand)
+    expect score.must_raise ArgumentError
   end
-
+  
   it 'raises an ArgumentError for scores over 21' do
-
+    hand = [3, 'Jack', 'Queen']
+    score = blackjack_score(hand)
+    expect score.must_raise ArgumentError
   end
 end
